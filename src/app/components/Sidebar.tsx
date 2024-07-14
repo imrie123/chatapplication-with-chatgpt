507484fca006c2cbd32ac49fa6ea5abf99288711
imrie123 <nosirie@ymobile.ne.jp>
2024-07-14 15:39:29 +0900

    Add react-loading-icons package for loading icons functionality.

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { BiLogOut } from "react-icons/bi";
+import { db } from "@/app/firebase";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  where,
+} from "firebase/firestore";
+import { useAppContext } from "../context/AppContext";
+
+type Room = {
+  id: string;
+  name: string;
+  createdAt: Timestamp;
+};
+
+const Sidebar = () => {
+  const { user, userId, setSelectedRoom } = useAppContext();
+  const [rooms, setRooms] = useState<Room[]>([]);
+  const selectRoom = (roomId: string) => {
+    setSelectedRoom(roomId);
+  };
+  useEffect(() => {
+    if (user) {
+      const fetchRooms = async () => {
+        const roomCollectionRef = collection(db, "rooms");
+        const q = query(
+          roomCollectionRef,
+          where("userId", "==", userId),
+          orderBy("createdAt")
+        );
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+          const newRooms: Room[] = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            name: doc.data().name,
+            createdAt: doc.data().createdAt,
+          }));
+          setRooms(newRooms);
+          console.log(newRooms);
+        });
+        return () => {
+          unsubscribe();
+        };
+      };
+      fetchRooms();
+    }
+  }, [userId]);
+
+  return (
+    <div className="bg-custom-blue h-full overflow-y-auto px-5 flex-col">
+      <div className="flex-grow">
+        <div className="cursor-pointer flex justify-evenly items-center border mt-2 rounded-md hover:bg-blue-800 duration-150">
+          <span className="text-white p-4 text-2xl">+</span>
+          <h1 className="text-white text-xl font-semibold p-4">New Chat</h1>
+        </div>
+        <ul>
+          {rooms.map((room) => (
+            <li
+              key={room.id}
+              className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700"
+              onClick={() => selectRoom(room.id)}
+            >
+              {room.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="text-lg flex items-center justify-evenly mb-2 cursor-pointer p-4 text-slate-100 hover:bg-slate-700 duration-150">
+        <BiLogOut />
+        <span>ログアウト</span>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
